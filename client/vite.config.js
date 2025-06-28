0d329ab630270178fd7636e5f505c5cbe141210f
Prakriti Sharma <prakritimksharma@gmail.com>
2025-06-28 19:38:19 +0530

    Trial hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Shim __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+    // root: 'client',
     plugins: [react()],
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
+    },
     server: {
         proxy: {
             '/api': {
